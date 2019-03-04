552e1e48aa41c2f2da1804dc7858b00b0e8e5f75
summerine <indri.pr.26@gmail.com>
2019-03-04 15:42:25 +0700

    create forms and migrate database

--- webapplication/crudapplication/models.py
+++ webapplication/crudapplication/models.py
@@ -5,4 +5,8 @@ class Employee(models.Model):
 	employee_id = models.CharField(max_length=20)
 	employee_name = models.CharField(max_length=100)
 	email = models.EmailField()
-	employee_contact = models.CharField(max_length=15, )
+	employee_contact = models.CharField(max_length=15)
+	class Meta:
+		db_table = "employee"
+
+		
