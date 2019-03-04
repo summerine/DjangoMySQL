4c0d9d016dd4ddd7d0c84e8b8613688fc7abee41
summerine <indri.pr.26@gmail.com>
2019-03-04 14:58:07 +0700

    initial commiy

---
+++ webapplication/crudapplication/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+	employee_id = models.CharField(max_length=20)
+	employee_name = models.CharField(max_length=100)
+	email = models.EmailField()
+	employee_contact = models.CharField(max_length=15, )
